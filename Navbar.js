7d053aa9dab69e6b6b80647f312acdb878827372
Abhishek Chauhan <87421954+29ab@users.noreply.github.com>
2021-12-15 21:02:38 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,22 @@
+function navbar(){
+    return `<div id="nav-bar" class="margin-mid">
+    <div id="groww-logo">
+        <img src="https://assets-netstorage.groww.in/web-assets/billion_groww_desktop/prod/build/client/images/logo-dark-groww.83f43714.svg" alt="Groww image">
+    </div>
+    <div id="after_logo">
+        <p>Explore</p>
+        <p>Investments</p>
+    </div>
+    <div id="search">
+        <img src="https://cdn-icons.flaticon.com/png/128/5948/premium/5948534.png?token=exp=1639478138~hmac=9a3448d12c73ff6effec0859d05105ba" alt="">
+        <input type="text" placeholder="What you are looking for today?" id="">
+    </div>
+    <div id="three-div">
+        <img src="https://cdn-icons-png.flaticon.com/128/3602/3602145.png" alt="">
+        <img src="https://img.icons8.com/ios/2x/wallet.png" alt="">
+        <img src="https://cdn-icons.flaticon.com/png/128/2252/premium/2252291.png?token=exp=1639483415~hmac=1364994abc22dcae9ce2f747eba82ff2" alt="">
+    </div>
+</div>`
+}
+
+export default navbar
